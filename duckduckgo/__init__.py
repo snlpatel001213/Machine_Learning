dbbb81e007c631c31050d322c793dc91ef948315
sunil <snlpatel001213@hotmail.com>
2016-08-31 17:40:25 +0530

    name recognization is more perfect now

--- duckduckgo/__init__.py
+++ duckduckgo/__init__.py
@@ -1,10 +1,21 @@
+import urllib
 from bs4 import BeautifulSoup
+url = "http://stellarpharma.tradeindia.com/paracetamol-tablets-500mg--654524.html"
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
 
-import requests
-import json
-query = 'PARACETAMOL+TABLETS'
-print 'http://duckduckgo.com/?q=' + query
-response = requests.get('http://duckduckgo.com/i.js?q='+query+"&t=h_&ia=web",verify=False)
-data = response.text
-data = json.loads(data)
-print data['results'][2]['url']
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+
+# get text
+text = soup.get_text()
+
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
+print(text)
--- nlp_framwork/nameRecog.py
+++ nlp_framwork/nameRecog.py
@@ -8,6 +8,8 @@ from collections import Counter
 import enchant
 import requests
 import json
+import urllib
+from bs4 import BeautifulSoup
 
 d = enchant.Dict("en_US")
 from nltk.corpus import stopwords
@@ -30,6 +32,7 @@ def duckSearch(searchList,numberOFSeraches):
     :return: list of urls
     """
     query = '+'.join(searchList)
+    query = query.replace(" ","+")
     print 'http://duckduckgo.com/?q=' + query
     response = requests.get('http://duckduckgo.com/i.js?q=' + query + "&t=h_&ia=web", verify=False)
     data = response.text
@@ -39,22 +42,37 @@ def duckSearch(searchList,numberOFSeraches):
         returnUrlsList.append(data['results'][i]['url'])
     return returnUrlsList
 
+def pageContentGrabber(url):
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()  # rip it out
+    # get text
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
 
 def nameEntitySpacy(multiSentence):
     parsedData = parser(multiSentence)
     sents = []
-    for span in parsedData.sents:
-        # go from the start to the end of each span, returning each token in the sentence
-        # combine each token using join()
-        sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
-        sents.append(sent)
+    # for span in parsedData.sents:
+    #     # go from the start to the end of each span, returning each token in the sentence
+    #     # combine each token using join()
+    #     sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
+    #     sents.append(sent)
 
     # for sentence in sents:
     #     print(sentence)
     #
-    for span in parsedData.sents:
-        sent = [parsedData[i] for i in range(span.start, span.end)]
-        break
+    # for span in parsedData.sents:
+    #     sent = [parsedData[i] for i in range(span.start, span.end)]
+    #     break
 
     # parsedEx = parsedData
     # # shown as: original token, dependency tag, head word, left dependents, right dependents
@@ -70,12 +88,21 @@ def nameEntitySpacy(multiSentence):
     print("-------------- entities only ---------------")
     # if you just want the entities and nothing else, you can do access the parsed examples "ents" property like this:
     ofInterest = []
-    ents = list(parsedData.ents)
-    for entity in ents:
-        # print(entity.label, entity.label_, ' '.join(t.orth_ for t in entity))
-        # if(entity.label_ == 'PERSON'):
-        ofInterest.append(' '.join(t.orth_ for t in entity))
-    print ofInterest
+
+    for span in parsedData.sents:
+        sent = [parsedData[i] for i in range(span.start, span.end)]
+        for token in sent:
+            if (token.pos_ == 'PROPN'):
+                ofInterest.append(token.orth_)
+
+
+
+    # ents = list(parsedData.ents)
+    # for entity in ents:
+    #     # print(entity.label, entity.label_, ' '.join(t.orth_ for t in entity))
+    #     # if(entity.label_ == 'PERSON'):
+    #     ofInterest.append(' '.join(t.orth_ for t in entity))
+    # print ofInterest
     return ofInterest
 
 
@@ -112,34 +139,67 @@ def ngrams(lst, n):
         else:
             break
 
+def chunkingAndCollocationFinder(multiSentence):
+    """
+    :param multiSentence: is text from html
+    :return: updates dictionary Nothing goes out
+    """
+    # multiSentence=unicode(multiSentence, "utf-8")
+    nonEnglishLine = " ".join(nameEntitySpacy(multiSentence))
+    print "nonEnglishLine"
+    print nonEnglishLine
+    words = re.findall("\w+", nonEnglishLine)
+    print Counter(ngrams(words, 2))
 
-multiSentence = u"""
-Rang De Basanti (IPA: [ˈrəŋɡ d̪eː bəˈsənt̪i]; English: Colour it Saffron) is a 2006 Indian drama film co written, co produced and directed by Rakeysh Omprakash Mehra. The literal meaning of the title can be translated as "Paint me with the colours of spring." It features an ensemble cast comprising Aamir Khan, Siddharth Narayan, Soha Ali Khan, Kunal Kapoor, R. Madhavan, Sharman Joshi, Atul Kulkarni and British actress Alice Patten in the lead roles. Made on a budget of ₹250 million (US$3.7 million), it was shot in and around New Delhi. Upon release, the film broke all opening box office records in India. It was the highest-grossing film in its opening weekend in India and had the highest opening day collections for a Bollywood film. The film was well received and praised for strong screenplay and dialogues.
+    parsedData = parser(multiSentence)
+    sents = []
+    for span in parsedData.sents:
+        sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
+        sents.append(sent)
+    directorSynonymes = ['director', 'directed', 'direction']
+    for i in sorted(Counter(ngrams(words, 2))):
+        # print " ".join(i) # got all the relevant names
+        for sent in sents:
+            for synonymes in directorSynonymes:
+                if (synonymes in sent.lower() and " ".join(i).lower() in sent.lower()):
+                    scrorer(" ".join(i))  # scoring
+                    print "director", " ".join(i)
 
-The story is about a British documentary filmmaker who is determined to make a film on Indian freedom fighters based on diary entries by her grandfather, a former officer of the British Indian Army. Upon arriving in India, she asks a group of five young men to act in her film.
+    for i in sorted(Counter(ngrams(words, 3))):
+        # print " ".join(i) # got all the relevant names
+        for sent in sents:
+            for synonymes in directorSynonymes:
+                if (synonymes in sent.lower() and " ".join(i).lower() in sent.lower()):
+                    scrorer(" ".join(i))  # scoring
+                    print "director", " ".join(i)
+    # print ratingDict
 
-Rang De Basanti's release faced stiff resistance from the Indian Defence Ministry and the Animal Welfare Board due to parts that depicted the use of MiG-21 fighter aircraft and a banned Indian horse race.
-
-The film was released globally on 26 January 2006, the Republic Day of India, it received critical acclaim winning National award for most popular film and it is also rated as 8.4 out of 10 on IMDB which is one of the highest among Bollywood films. It was subsequently nominated for Best Foreign Language Film at the 2006 BAFTA Awards. Rang De Basanti was chosen as India's official entry for the Golden Globe Awards and the Academy Awards in the Best Foreign Language Film category, though it did not ultimately yield a nomination for either award. A. R. Rahman's soundtrack, which earned positive reviews, had two of its tracks considered for the Academy Award nomination. The film was well received by critics and audiences for its production values and had a noticeable influence on Indian society. In India, Rang De Basanti did well at many of the Bollywood awards ceremonies, including a win for Best Movie at the Filmfare Awards. The film was declared "Blockbuster" by Box Office India.
-"""
-# print nameEntitySpacy(multiSentence)[0]
 
 
-
-nonEnglishLine = " ".join(nameEntitySpacy(multiSentence))
-words = re.findall("\w+", nonEnglishLine)
-print Counter(ngrams(words, 2))
-parsedData = parser(multiSentence)
-sents = []
-for span in parsedData.sents:
-    # go from the start to the end of each span, returning each token in the sentence
-    # combine each token using join()
-    sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
-    sents.append(sent)
-for i in sorted(Counter(ngrams(words, 2))):
-    # print " ".join(i) # got all the relevant names
-    for sent in sents:
-        if ("directed" in sent.lower() and " ".join(i).lower() in sent.lower()):
-            scrorer(" ".join(i))
-            print "director", " ".join(i)
+###############Start##############################
+#Sarching for 'director' and 'movie'
+InitialSearch = ['Director of','rustom movie']
+InitialSearchResultURLS = duckSearch(InitialSearch,3)
+print InitialSearchResultURLS
+for url in InitialSearchResultURLS:
+    pageContent=pageContentGrabber(url)
+    print pageContent
+    chunkingAndCollocationFinder(pageContent)
+###############AdvanceSearch######################
+#SecondSearch
+for key in ratingDict:
+    InitialSearch = ['Director of','rustom',key] #"key is the name of the person who is supposed to be director"
+    InitialSearchResultURLS = duckSearch(InitialSearch,3)
+    for url in InitialSearchResultURLS:
+        pageContent=pageContentGrabber(url)
+        chunkingAndCollocationFinder(pageContent)
 print ratingDict
+
+
+
+
+
+
+
+
+
